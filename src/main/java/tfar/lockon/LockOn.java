7bedd10e12c52205c39732024204674bdc4732e4
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2020-09-22 19:09:30 -0400

    finished

---
+++ src/main/java/tfar/lockon/LockOn.java
@@ -0,0 +1,70 @@
+package tfar.lockon;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileHelper;
+import net.minecraft.util.EntityPredicates;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.Optional;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(LockOn.MODID)
+public class LockOn {
+    // Directly reference a log4j logger.
+
+    public static final String MODID = "lockon";
+
+    public LockOn() {
+        if (FMLEnvironment.dist == Dist.CLIENT)
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(LockOnHandler::client);
+    }
+
+    /*public static Optional<Entity> rayTrace(PlayerEntity player) {
+        double distance = 64;
+        Vector3d vector3d = player.getEyePosition(1);
+        Vector3d vector3d1 = player.getLook(1.0F);
+        Vector3d vector3d2 = vector3d.add(vector3d1.x * distance, vector3d1.y * distance, vector3d1.z * distance);
+        Entity pointedEntity = null;
+        List<Entity> list = player.world.getEntitiesInAABBexcluding(player, player.getBoundingBox()
+                        .expand(vector3d1.x * distance, vector3d1.y * distance, vector3d1.z * distance).grow(1.0D, 1.0D, 1.0D),
+                EntityPredicates.NOT_SPECTATING);
+        double d2 = distance;
+
+        for (int j = 0; j < list.size(); ++j) {
+            Entity entity1 = list.get(j);
+            AxisAlignedBB axisalignedbb = entity1.getBoundingBox().grow(entity1.getCollisionBorderSize());
+            EntityRayTraceResult entityraytraceresult = ProjectileHelper.rayTraceEntities(player.world, player, vector3d, vector3d2, axisalignedbb,
+                    (entity) -> !entity.isSpectator() && entity.canBeCollidedWith());
+            if (axisalignedbb.contains(vector3d)) {
+                if (d2 >= 0.0D) {
+                    pointedEntity = entity1;
+                    d2 = 0.0D;
+                }
+            } else if (entityraytraceresult != null) {
+                double d3 = vector3d.distanceTo(entityraytraceresult.getHitVec());
+
+                if (d3 < d2 || d2 == 0.0D) {
+                    if (entity1.getLowestRidingEntity() == player.getLowestRidingEntity() && !entity1.canRiderInteract()) {
+                        if (d2 == 0.0D) {
+                            pointedEntity = entity1;
+                        }
+                    } else {
+                        pointedEntity = entity1;
+                        d2 = d3;
+                    }
+                }
+            }
+        }
+        return Optional.ofNullable(pointedEntity);
+    }*/
+}
---
+++ src/main/java/tfar/lockon/LockOnHandler.java
@@ -0,0 +1,142 @@
+package tfar.lockon;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.EntityPredicate;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3d;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+import static net.minecraftforge.common.MinecraftForge.EVENT_BUS;
+import static tfar.lockon.LockOn.MODID;
+
+
+public class LockOnHandler {
+
+    public static KeyBinding LOCK_ON;
+    public static KeyBinding TAB;
+
+    public static List<LivingEntity> list = new ArrayList<>();
+
+    private static final Minecraft mc = Minecraft.getInstance();
+
+    public static void client(FMLClientSetupEvent e) {
+        LOCK_ON = new KeyBinding("key." + MODID + ".lock_on", GLFW.GLFW_KEY_O, "key.categories." + MODID);
+        TAB = new KeyBinding("key." + MODID + ".tab", GLFW.GLFW_KEY_TAB, "key.categories." + MODID);
+        ClientRegistry.registerKeyBinding(LOCK_ON);
+        ClientRegistry.registerKeyBinding(TAB);
+        EVENT_BUS.addListener(LockOnHandler::handleKeyPress);
+    }
+
+    public static boolean lockedOn;
+    private static Entity targetted;
+
+    private static void handleKeyPress(TickEvent.RenderTickEvent e) {
+        PlayerEntity player = mc.player;
+        if (e.phase == TickEvent.Phase.START && mc.player != null && !mc.isGamePaused()) {
+            tickLockedOn();
+            while (LOCK_ON.isPressed()) {
+                if (lockedOn) {
+                    leaveLockOn();
+                } else {
+                    attemptEnterLockOn(player);
+                }
+            }
+
+            while (TAB.isPressed()) {
+                tabToNextEnemy(player);
+            }
+
+            if (targetted != null) {
+                Vector3d targetPos = targetted.getPositionVec();
+                Vector3d directionVec = targetPos.subtract(mc.player.getPositionVec()).normalize();
+                double angle = Math.atan2(-directionVec.x, directionVec.z) * 180 / Math.PI;
+
+                float adjustedPrevYaw = mc.player.prevRotationYaw;
+                if (Math.abs(angle - adjustedPrevYaw) > 180) {
+                    if (adjustedPrevYaw > angle) {
+                        angle += 360;
+                    } else if (adjustedPrevYaw < angle) {
+                        angle -= 360;
+                    }
+                }
+
+                double newDelta = MathHelper.lerp(e.renderTickTime, adjustedPrevYaw, angle);
+                if (newDelta > 180) {
+                    newDelta -= 360;
+                }
+                if (newDelta < -180) {
+                    newDelta += 360;
+                }
+                mc.player.rotationYaw = (float)newDelta;
+            }
+        }
+    }
+
+    private static void attemptEnterLockOn(PlayerEntity player) {
+        tabToNextEnemy(player);
+        if (targetted != null) {
+            lockedOn = true;
+        }
+    }
+
+    private static void tickLockedOn() {
+        list.removeIf(livingEntity -> mc.player == null || !livingEntity.isAlive());
+        if (targetted != null && !targetted.isAlive()) {
+            targetted = null;
+            lockedOn = false;
+        }
+    }
+
+    private static final Predicate<LivingEntity> ENTITY_PREDICATE = entity -> entity.isAlive() && entity.attackable();
+    private static final EntityPredicate ENEMY_CONDITION = new EntityPredicate().setDistance(20.0D).setCustomPredicate(ENTITY_PREDICATE);
+
+    private static int cycle = -1;
+
+    public static Entity findNearby(PlayerEntity player) {
+        List<LivingEntity> entities = player.world.getTargettableEntitiesWithinAABB(LivingEntity.class, ENEMY_CONDITION, player, player.getBoundingBox().grow(10.0D, 2.0D, 10.0D));
+        if (lockedOn) {
+            cycle++;
+            for (LivingEntity entity : entities) {
+                if (!list.contains(entity)) {
+                    list.add(entity);
+                    return entity;
+                }
+            }
+
+           //cycle existing entity
+            if (cycle >= list.size()) {
+                cycle = 0;
+            }
+            return list.get(cycle);
+        } else {
+            if (!entities.isEmpty()) {
+                LivingEntity first = entities.get(0);
+                list.add(first);
+                return entities.get(0);
+            } else {
+                return null;
+            }
+        }
+    }
+
+    private static void tabToNextEnemy(PlayerEntity player) {
+        targetted = findNearby(player);
+    }
+
+    private static void leaveLockOn() {
+        targetted = null;
+        lockedOn = false;
+        list.clear();
+    }
+}
